649ab132583fce8f1a2a9a0ee8502d680c8d43c1
Joshua Partogi <proscrumtrainer@live.com>
2014-10-07 12:08:57 +0800

    change to relative path include in bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -2,7 +2,7 @@
 
 spl_autoload_register(function ($class_name) {
     $class_name =  str_replace('\\', '/', $class_name);
-    require_once  __DIR__ . '/app/' . $class_name . '.php';
+    require_once  'app/' . $class_name . '.php';
 });
 
-require_once __DIR__ . '/tests/DOSBox/DOSBoxTestCase.php';
+require_once 'tests/DOSBox/DOSBoxTestCase.php';
